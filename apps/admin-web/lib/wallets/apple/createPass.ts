f91402b4c2760a1bf77dac85047daab6dcb0ef20
Andres Felipe Valencia Benitez <afvalenciab@gmail.com>
2025-07-24 22:01:08 -0600

    fix(LKR-36): Change create Apple pass from folder model to buffer model

--- apps/admin-web/lib/wallets/apple/createPass.ts
+++ apps/admin-web/lib/wallets/apple/createPass.ts
@@ -1,20 +1,11 @@
-import path from "node:path";
 import { baseAppUrlEnv } from "@/lib/utils/environmentValues";
 import {
   appleCertificatePemBase64Env,
   appleKeyPemBase64Env,
   appleWwdrPemBase64Env,
 } from "@/lib/utils/environmentValues";
-import { PKPass } from "passkit-generator";
 
 export async function createLoyaltyPass(_storeName: string): Promise<Buffer> {
-  // Usamos una ruta relativa al propio archivo compilado para que el directorio del
-  // template se incluya correctamente en el bundle (especialmente en entornos
-  // serverless como Vercel). __dirname apunta a apps/admin-web/lib/wallets/apple/*
-  // en el artefacto final, por lo que simplemente navegamos hasta la carpeta
-  // "templates/loyalty.pass" que vive junto a este archivo.
-  // const modelDir = path.join(__dirname, "templates", "loyalty.pass");
-
   const certificates = {
     wwdr: Buffer.from(appleWwdrPemBase64Env || "", "base64"),
     signerCert: Buffer.from(appleCertificatePemBase64Env || "", "base64"),
@@ -22,14 +13,38 @@ export async function createLoyaltyPass(_storeName: string): Promise<Buffer> {
     signerKeyPassphrase: process.env.APPLE_CERT_PASSWORD || "",
   };
 
-  const pass = await PKPass.from({ model: "./templates/loyalty.pass", certificates });
-  // const pass = await PKPass.from({ model: modelDir, certificates });
+  const { PKPass } = await import("passkit-generator");
+
+  const pass = new PKPass({}, certificates, {
+    description: "Example Apple Wallet Pass",
+    passTypeIdentifier: "pass.com.passkitgenerator",
+    serialNumber: "example",
+    organizationName: "Lynkeer",
+    teamIdentifier: "example",
+    foregroundColor: "#000",
+    labelColor: "#000",
+    backgroundColor: "#fff",
+  });
+
+  // pass.type = "storeCard";
+  // pass.headerFields.push(
+  //   {
+  //     key: "header-field-test-1",
+  //     value: "Unknown",
+  //   },
+  //   {
+  //     key: "header-field-test-2",
+  //     value: "unknown",
+  //   },
+  // );
+  // pass.addBuffer("icon.png", Buffer.from(iconFromModel));
+  // pass.addBuffer("icon@2x.png", Buffer.from(iconFromModel));
+  // pass.addBuffer("icon@3x.png", Buffer.from(iconFromModel));
 
   const stripUrl = `${baseAppUrlEnv}/api/wallet/stamps/9/1/stamps.png`;
   const imageResponse = await fetch(stripUrl);
 
   const stripImage = await imageResponse.arrayBuffer();
-
   pass.addBuffer("strip.png", Buffer.from(stripImage));
 
   return await pass.getAsBuffer();
